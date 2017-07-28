738e84554988d55d724de86171bbf1cd3eb0055a
Dan Mello <jdanmello@gmail.com>
2017-07-28 16:22:50 -0400

    Setting up application added ejs, and favicon

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-const express = require('express');
+const express = require('express')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const favicon = require('serve-favicon')
+const faviconPath = __dirname + "/public/nodejs.ico"
 
 express()
+    .set('view engine', 'ejs')
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({extended: false}))
+    .use(favicon(faviconPath))
     .get('/', (req, res, next) => {
-    	console.log("request received")
-    	res.send("ok")
+
+        res.render('pages/index')
+
+        console.log("request received")
+        
     })
     .listen(3000)
