88876cf1eda6b06546528ecc17412fd1c80cdd0d
Dan Mello <jdanmello@gmail.com>
2017-07-30 20:30:10 -0400

    testing database connection and adding some static content

--- index.js
+++ index.js
@@ -2,18 +2,39 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const session = require('express-session')
 const favicon = require('serve-favicon')
-const faviconPath = __dirname + "/public/nodejs.ico"
+const staticAssets = __dirname + '/public'
+const faviconPath = __dirname + "/public/favicon.ico"
+const knex = require('knex')
+
+const db = knex({
+    client: 'mysql',
+    connection: {
+        host: '172.16.60.134',
+        user: 'deploy',
+        password: 'test',
+        database: 'nodejs_app',
+    }
+})
 
 express()
     .set('view engine', 'ejs')
     .use(bodyParser.json())
     .use(bodyParser.urlencoded({extended: false}))
+    .use(express.static(staticAssets))
     .use(favicon(faviconPath))
     .get('/', (req, res, next) => {
 
-        res.render('pages/index')
+        db('users').then((users) => {
+            res.send(users)
+        })
+        .catch((error) => {
+            res.send(error)
+        })
 
         console.log("request received")
         
+    })
+    .get('/static', (req, res, next) => {
+        res.render('pages/index')
     })
     .listen(3000)
