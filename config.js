f57b65f3c691990a459461f88be58a6055e04ade
Dan Mello <jdanmello@gmail.com>
2018-07-01 09:33:29 -0400

    Setting up production deployments

--- config.js
+++ config.js
@@ -42,7 +42,7 @@ exports = module.exports = function(app) {
           host: '10.0.0.201', // local ip or public ip if im not connect to local connection
           ref: 'origin/master',
           repo: 'https://github.com/DanMello/jdanmello.com.git',
-          path: '/home/deploy/deployConfig/jdanmello.com',
+          path: '/home/deploy/jdanmello.com',
           'post-deploy' : 'nvm install && npm install && /home/deploy/.nvm/versions/node/v8.11.3/bin/pm2 reload ecosystem.config.js --env production'
         }
       }
